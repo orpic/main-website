7674c028bf7f2efc6ad63e23675bd579e7b53c31
shobhit <shobhitumrao5@gmail.com>
2023-08-11 10:28:41 +0530

    Auto-commit:
    $n   M src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello Shobhit 3</h1>
+      <h1 className="text-3xl font-bold underline">Hello Shobhit test</h1>
     </>
   );
 }
