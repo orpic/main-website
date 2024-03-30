44f75a577680cea5e4bda3ab821e181397c07285
Shobhit Umrao <shobhitumrao5@gmail.com>
2024-03-30 11:09:14 +0530

    privacy policy updated

--- src/pages/Privacy.tsx
+++ src/pages/Privacy.tsx
@@ -1,5 +1,17 @@
 const Privacy = () => {
-  return <div>No data is collected.</div>;
+  return (
+    <>
+      <h1>Privacy policy</h1>
+      <p>The deveoper here in Shobhit Umrao is the app you are using.</p>
+      <p></p>
+      <p>No data is collected at any point in time. This is a static app.</p>
+      <p>
+        Third party data collections also not present, but if any eg Google, Ms
+        Clarity, or any other tracking they collect is anonymised data and how
+        hey use it is governed by their respective privacy policy.
+      </p>
+    </>
+  );
 };
 
 export default Privacy;
